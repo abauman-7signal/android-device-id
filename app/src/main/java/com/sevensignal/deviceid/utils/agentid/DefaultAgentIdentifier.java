dbd7bc4154216112e4e4dd832db216a4e8ab92e9
Aaron Bauman <aaron.bauman@7signal.com>
2021-07-07 09:47:51 -0400

    green: creates UUID each time an ID is requested

--- app/src/main/java/com/sevensignal/deviceid/utils/agentid/DefaultAgentIdentifier.java
+++ app/src/main/java/com/sevensignal/deviceid/utils/agentid/DefaultAgentIdentifier.java
@@ -2,9 +2,11 @@ package com.sevensignal.deviceid.utils.agentid;
 
 import com.sevensignal.deviceid.exceptions.AgentIdentifierStoreException;
 
+import java.util.UUID;
+
 public class DefaultAgentIdentifier implements AgentIdentifier {
 	@Override
 	public String getId() throws AgentIdentifierStoreException {
-		return "an id";
+		return UUID.randomUUID().toString().trim();
 	}
 }
